c078d4c5d1b6930d532fa2d871a1d5853db35234
SohelShahid117 <sohelshahid09@gmail.com>
2025-03-15 21:28:08 +0600

    8-3 Installing eslint, refactor code, fix errors using command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+import express, { Application, Request, Response } from "express"
+import cors from "cors"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app:Application = express()
+// const port = 3000
+
+//parser
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req:Request, res:Response) => {
+  // const a = 10
+  const a = 10
+  // res.send('Hello World!')
+  res.send(a)
 })
 
 console.log(process.cwd())
+
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,35 @@
+import app from "./app";
+import config from "./app/config";
+
 // getting-started.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+import mongoose from "mongoose"
 
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
+  try{
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  }
+  catch(err){
+    console.log(err)
+  }
+  // await mongoose.connect(process.env.DATABASE_URL);
+  // await mongoose.connect(config.database_url as string);
   // await mongoose.connect('mongodb://127.0.0.1:27017/test');
   //mongodb+srv://sohelcuet09:<db_password>@cluster0.el2ir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  // app.listen(config.port, () => {
+  //   console.log(`Example app listening on port ${config.port}`)
+  // })
 }
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+// app.listen(port, () => {
+// app.listen(config.port, () => {
+//     console.log(`Example app listening on port ${config.port}`)
+//   })
+
+main()
