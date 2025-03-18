35bc9156604189dee77172f1de7a267e05f3f49e
SohelShahid117 <sohelshahid09@gmail.com>
2025-03-18 09:56:41 +0600

    done 8-4 Install prettier,ts-node-dev,fix formatting issues

--- dist/server.js
+++ dist/server.js
@@ -8,17 +8,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+const config_1 = __importDefault(require("./app/config"));
 // getting-started.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const mongoose_1 = __importDefault(require("mongoose"));
 main().catch(err => console.log(err));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        yield mongoose.connect(process.env.DATABASE_URL);
+        try {
+            yield mongoose_1.default.connect(config_1.default.database_url);
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
+        // await mongoose.connect(process.env.DATABASE_URL);
+        // await mongoose.connect(config.database_url as string);
         // await mongoose.connect('mongodb://127.0.0.1:27017/test');
         //mongodb+srv://sohelcuet09:<db_password>@cluster0.el2ir.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
         // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+        // app.listen(config.port, () => {
+        //   console.log(`Example app listening on port ${config.port}`)
+        // })
     });
 }
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+// app.listen(config.port, () => {
+//     console.log(`Example app listening on port ${config.port}`)
+//   })
+main();
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
 // const express = require('express')
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-const app:Application = express()
+const app: Application = express();
 // const port = 3000
 
 //parser
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
+app.get("/", (req: Request, res: Response) => {
+  const a = 10;
   // const a = 10
-  const a = 10
   // res.send('Hello World!')
-  res.send(a)
-})
+  res.send(a);
+});
 
-console.log(process.cwd())
 
-export default app
+
+
+
+
+
+console.log(process.cwd());
+
+export default app;
