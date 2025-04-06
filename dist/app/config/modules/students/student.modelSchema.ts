3fda3242b3aeb9394eaa8a1898103c54c4486e7e
SohelShahid117 <sohelshahid09@gmail.com>
2025-04-06 15:31:43 +0600

    done 8-7 Refactor your schema

--- dist/app/config/modules/students/student.modelSchema.ts
+++ dist/app/config/modules/students/student.modelSchema.ts
@@ -1,5 +1,5 @@
 import { Schema, model, connect } from 'mongoose';
-import { Student } from './student.interface';
+import { Guardian, LocalGuardian, Student, UserName } from './student.interface';
 
 // 2. Create a Schema corresponding to the document interface.
 /*const userSchema = new Schema<IUser>({
@@ -9,13 +9,31 @@ import { Student } from './student.interface';
 });
 */
 
-const studentSchema = new Schema<Student>({
-     id: {type:String,required:true},
-     name:{
+const userNameSchema = new Schema<UserName>({
         firstName:{type:String,required:true},
         middleName:{type:String},
-        lastName:{type:String,required:true},
-      },
+        lastName:{type:String,required:true}
+})
+
+const guardianSchema = new Schema<Guardian>({
+        fatherName: {type:String,required:true},
+        fatherOccupation: {type:String,required:true},
+        fatherContactNumber: {type:String,required:true},
+        motherName: {type:String,required:true},
+        motherOccupation: {type:String,required:true},
+        motherContactNumber: {type:String,required:true},
+})
+
+const localGuardianSchema = new Schema<LocalGuardian>({
+        name: {type:String,required:true},
+        occupation: {type:String,required:true},
+        contactNumber: {type:String,required:true},
+        address: {type:String,required:true},
+     })
+
+const studentSchema = new Schema<Student>({
+     id: {type:String,required:true},
+     name:userNameSchema,
       //   gender: string;
       gender:['male','female'] , //this is enum type in TS
       dateOfBirth: {type:String},
@@ -26,21 +44,15 @@ const studentSchema = new Schema<Student>({
       bloodGroup:[ 'A+' , 'A-' , 'B+' , 'B-' , 'AB+' , 'AB-' , 'O+' , 'O-'];
       presentAddress: {type:String,required:true},
       permanentAddress: {type:String,required:true},
-      guardian: {
-        fatherName: {type:String,required:true},
-        fatherOccupation: {type:String,required:true},
-        fatherContactNumber: {type:String,required:true},
-        motherName: {type:String,required:true},
-        motherOccupation: {type:String,required:true},
-        motherContactNumber: {type:String,required:true},
-     },
-     localGuardian:{
-        name: {type:String,required:true},
-        occupation: {type:String,required:true},
-        contactNumber: {type:String,required:true},
-        address: {type:String,required:true},
-     },
-     profileImg: {type:String,required:true},
+      guardian: guardianSchema,
+      localGuardian:localGuardianSchema,
+      profileImg: {type:String,required:true},
     //  isActive:['active','inActive']
      isActive:['active','blocked']
 });
+
+
+// 3. Create a Model.
+// const User = model<IUser>('User', userSchema);
+
+const Student = model<UserName>("Student",studentSchema)
